78a5e2c51607fecbe83519a2ae57422794ebe5fc
luperciofferraz <lupercio.ferraz@gmail.com>
2021-05-01 12:41:37 -0300

    Navigation

--- App.tsx
+++ App.tsx
@@ -1,60 +1,18 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 const App = () => {
 
   return (
 
-    <View style={styles.container}>
+    <NavigationContainer>
 
-      <Text style={styles.h1}>Alo DevSuperior</Text>
-      <Text style={styles.h2}>Lupércio</Text>
-      <Text style={styles.h3}>Ferraz</Text>
+      
+    </NavigationContainer>
 
-    </View>
   )
 
 }
-
-const styles = StyleSheet.create({
-
-  container: {
-
-    //default: DisplayFlex
-
-    backgroundColor: "#069",
-    flex: 1,
-    flexDirection: "column", //Default: column
-    alignItems: "center", //Alinhamento Horizontal
-    justifyContent: "center" //Alinhamento Vertical
-
-  },
-
-  h1: {
-
-    fontSize: 24,
-    fontWeight: "700",
-    color: "#fff"
-
-  },
-  
-  h2: {
-
-    fontSize: 18,
-    fontWeight: "500",
-    color: "#fff"
-
-  },
-  
-  h3: {
-
-    fontSize: 14,
-    fontWeight: "700",
-    color: "#ccc"
-
-  }
-
-});
-
 
 export default App;
