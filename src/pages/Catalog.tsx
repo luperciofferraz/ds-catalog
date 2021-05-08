57825d4b9de27cd65bf35eb2f4e3504eac8dc41c
luperciofferraz <lupercio.ferraz@gmail.com>
2021-05-07 21:38:32 -0300

    Product Search

--- src/pages/Catalog.tsx
+++ src/pages/Catalog.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ScrollView } from 'react-native';
-import { ProductCard } from '../components';
+import { ProductCard, SearchInput } from '../components';
 
 import productImg from  '../assets/produto.png';
 import { theme } from '../styles';
@@ -50,11 +50,17 @@ const products = [
 
 const Catalog: React.FC = () => {
 
+    const [search, setSearch] = useState("");
+
+    const data = search.length > 0 ? products.filter(product => product.name.toLocaleLowerCase().includes(search.toLowerCase())) : products;
+
     return (
 
         <ScrollView contentContainerStyle={theme.scrollContainer}>
 
-            {products.map((product) => (
+            <SearchInput placeholder="Nome do produto" search setSearch={setSearch} />
+
+            {data.map((product) => (
 
                 <ProductCard { ... product} />
 
