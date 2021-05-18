2100aeb185125eb91f5d1900c44dee41c3aece17
luperciofferraz <lupercio.ferraz@gmail.com>
2021-05-18 02:05:32 -0300

    NavBar

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,18 +1,37 @@
 import React from 'react';
+import { Text } from 'react-native';
 import { Home, Catalog, ProductDetails } from '../pages';
 import { createStackNavigator } from '@react-navigation/stack';
+import { colors, nav } from '../styles';
+import { NavBar } from '../components';
 
 const Stack = createStackNavigator();
+
+const HeaderText: React.FC = () => <Text style={nav.leftText}>Ds Catalog</Text>
 
 const Routes: React.FC = () => {
 
     return (
         
-        <Stack.Navigator>
+        <Stack.Navigator 
+        
+            screenOptions={{ 
 
-            <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
-            <Stack.Screen name="Catalog" component={Catalog} options={{ headerShown: false }} />
-            <Stack.Screen name="ProductDetails" component={ProductDetails} options={{ headerShown: false }} />
+                headerTitle: " ",
+                headerStyle: {
+
+                    backgroundColor: colors.primary,
+                },
+                
+                headerLeft: () => <HeaderText />,
+                headerRight: () => <NavBar />
+
+            }} 
+        >    
+
+            <Stack.Screen name="Home" component={Home} />
+            <Stack.Screen name="Catalog" component={Catalog} />
+            <Stack.Screen name="ProductDetails" component={ProductDetails} />
 
         </Stack.Navigator>
     );
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,4 +1,6 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
+
+const deviceWidth = Dimensions.get('window').width;
 
 const colors = {
 
@@ -92,7 +94,7 @@ const text = StyleSheet.create({
         color: colors.mediumGray,
         fontWeight: "400",
 
-    }
+    },
 
 });
 
@@ -313,4 +315,51 @@ const theme = StyleSheet.create({
 
 });
 
-export { colors, theme, text };
+const nav = StyleSheet.create({
+
+    leftText: {
+
+        color: colors.white,
+        fontWeight: "bold",
+        marginLeft: 20,
+
+    },
+
+    drawer: {
+
+        marginRight: 20,
+    },
+
+    options: {
+
+        width: deviceWidth,
+        height: 120,
+        backgroundColor: colors.primary,
+        marginTop: 125,
+        marginRight: -20,
+        padding: 20,
+        justifyContent: "space-between"
+
+    },
+
+    option: {
+
+        paddingVertical: 5,
+    },
+
+    textOption: {
+
+        color: colors.white,
+        textTransform: "uppercase",
+
+    },
+
+    textActive: {
+
+        fontWeight: "bold",
+    }
+
+
+});
+
+export { colors, theme, text, nav };
